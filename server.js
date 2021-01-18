7a9274d2e8bfd93bd14cd506824b0b9de5f78df9
Vivaldek <dawidg25@gmail.com>
2021-01-18 01:19:58 +0100

    https revert

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 require('dotenv').config();
@@ -10,7 +8,6 @@ require('dotenv').config();
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
 
 require('./db');
 
@@ -18,17 +15,10 @@ const PostsController = require('./api/posts/PostsController');
 
 app.use('/posts', PostsController);
 
-const options = {
-	key: fs.readFileSync('./key.pem'),
-	cert: fs.readFileSync('./cert.pem')
-};
-
-const server = https.createServer(options, app);
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-server.listen(port, () => {
-	console.log(`Server is running on port 8080`);
+app.listen(8080, () => {
+    console.log('App listen on port 8080')
 });

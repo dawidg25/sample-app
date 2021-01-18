18d1de7bc09dac5828b3979e40febeb5ae9d9adc
Vivaldek <dawidg25@gmail.com>
2021-01-18 01:12:34 +0100

    https

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -15,8 +16,17 @@ const PostsController = require('./api/posts/PostsController');
 
 app.use('/posts', PostsController);
 
+const options = {
+	key: fs.readFileSync('./key.pem'),
+	cert: fs.readFileSync('./cert.pem')
+};
+
+const server = https.createServer(options, app);
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+server.listen(port, () => {
+	console.log(`Server is running on port 8080`);
+});
